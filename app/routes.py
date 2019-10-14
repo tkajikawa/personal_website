f788347739640c43cdb6d0f7cb76af01a5f210b1
trentk <tkajikaw@usc.edu>
2019-10-14 10:15:10 -0700

    add infrastructure scripts

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from app import app
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+@app.route("/")
+@app.route('/home')
+def home():
+    return render_template('home.html')
