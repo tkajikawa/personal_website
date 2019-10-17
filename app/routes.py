b8ad40d8ed027f6887ba4ed5a98ee57f900b1371
tkajikawa <tmkajikawa@gmail.com>
2019-10-17 11:44:38 -0700

    add more html

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,14 @@ from flask import render_template
 from app import app
 
 @app.route("/")
-@app.route('/index.html')
+@app.route('/index/')
 def home():
     return render_template('index.html')
+
+@app.route('/aboutme/')
+def aboutme():
+    return render_template('aboutme.html')
+
+@app.route('/music/')
+def music():
+    return render_template('music.html')
