337456649f5130f5ddc649e830903b0d1a188ad7
Trent K <tmkajikawa@gmail.com>
2019-10-27 20:28:05 -0700

    make updates to flask website

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,18 @@
 from flask import render_template, request, url_for, redirect
 from app import app
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
-    if request.method == 'POST':
-        # do stuff when the form is submitted
-
-        # redirect to end the POST handling
-        # the redirect can be to the same route or somewhere else
-        return redirect(url_for('aboutme.html'))
     return render_template('index.html')
 
 @app.route('/aboutme/')
 def aboutme():
-    return render_template('aboutme.html')
+    return render_template('index.html')
 
 @app.route('/music/')
 def music():
     return render_template('music.html')
+
+@app.route('/more/')
+def more():
+    return render_template('more.html')
